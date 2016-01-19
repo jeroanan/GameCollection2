a903fcbe5775276f454f785046e780455f53fd07
jeroanan <david.tosslehoff@gmail.com>
2016-01-19 23:27:41 +0000

    Init platforms table

--- dbinit.go
+++ dbinit.go
@@ -135,19 +135,55 @@ func populateMasterTables(db *sql.DB) {
 }
 
 func populatePlatformsTable(db *sql.DB) {
-  _ = LoadPlatformsMasterFile()
 
-  tx, err = db.begin()
+  log.Print("Populating Platform table from masterfiles/platforms.json")
 
-  if err!=nil {
-    log.Fatal(err)
-  }
+  platformData := LoadPlatformsMasterFile()
 
-  existQuery, err := "SELECT COUNT(Name) FROM Platforms WHERE Name=?"
+  tx, err := db.Begin()
 
   if err!=nil {
     log.Fatal(err)
   }
 
-  
+  existQuery, err := tx.Prepare("SELECT COUNT(Name) FROM Platform WHERE Name=?")
+
+  if err!=nil {
+    log.Fatal(err)
+  }
+
+  defer existQuery.Close()
+
+  var platformsToAdd []Platform
+
+  for _, platform := range(platformData.Platforms) {
+    rs, err := existQuery.Query(platform.Name)
+
+    if err!=nil {
+      log.Fatal(err)
+    }
+
+    var count int
+    for rs.Next() {
+      rs.Scan(&count)
+    }
+
+    if count==0 {
+      platformsToAdd = append(platformsToAdd, platform)
+    }
+  }
+
+  insertStmt, err := tx.Prepare("INSERT INTO Platform (Name, Description) VALUES (?, ?)")
+
+  defer insertStmt.Close()
+
+  if err!=nil {
+    log.Fatal(err)
+  }
+
+  for _, platform := range(platformsToAdd) {
+    log.Printf("Inserting platform %s (%s)", platform.Name, platform.Description)
+    insertStmt.Exec(platform.Name, platform.Description)
+  }
+  tx.Commit()
 }
